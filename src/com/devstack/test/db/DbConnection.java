d3a35538f8aea8dffc80ed534d5d5a64742b2ba1
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2024-07-16 22:05:58 +0530

    created DbConnection.java

---
+++ src/com/devstack/test/db/DbConnection.java
@@ -0,0 +1,23 @@
+package com.devstack.test.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+    private Connection connection = null;
+    private static DbConnection dbConnection;
+
+    private DbConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/dsmp_3_test","root", "1234"
+        );
+    }
+    private static DbConnection getInstance() throws SQLException, ClassNotFoundException {
+        return (dbConnection == null)?dbConnection=new DbConnection():dbConnection;
+    }
+    public Connection getConnection(){
+        return connection;
+    }
+}
